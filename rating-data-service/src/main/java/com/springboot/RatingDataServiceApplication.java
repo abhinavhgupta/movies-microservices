5953c143a350c8d4b6ade7be1db23d8baddea3b1
Abhinav Gupta <Abhinav.Gupta>
2020-01-08 23:44:22 +0530

    06. Added Rating service API

--- rating-data-service/src/main/java/com/springboot/RatingDataServiceApplication.java
+++ rating-data-service/src/main/java/com/springboot/RatingDataServiceApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.ratingdataservice;
+package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
